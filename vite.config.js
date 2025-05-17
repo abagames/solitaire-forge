2f7eeab9970f078bc712025c22591a0fd0edac01
abagames <cs8k-cyu@asahi-net.or.jp>
2025-05-17 10:53:36 +0900

    Change base directory

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   // Change the directory name according to the game being developed.
   root: "src/games/catalyst-reaction",
+  base: "./",
   build: {
     // Change the directory name according to the game being developed.
     outDir: "../../../docs/catalyst-reaction",
